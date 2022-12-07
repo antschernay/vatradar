a41557fb10017b9b2eade772f4d835daa2431c69
Aneta Tschernay <86184090+antschernay@users.noreply.github.com>
2022-12-07 10:06:59 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker} from 'react-leaflet';
-import L from "leaflet";
+import { MapContainer, TileLayer} from 'react-leaflet';
 import '../node_modules/leaflet/dist/leaflet.css'
 import PlaneList from "./PlaneList";
 
@@ -9,31 +8,30 @@ import PlaneList from "./PlaneList";
 
 function App() {
 
-
-  const jsonUrl = 'https://data.vatsim.net/v3/vatsim-data.json';
-
   const [pilots, setData] = useState([]);
+ 
 
-
-  useEffect(() => {
-    fetch(jsonUrl)
+  function fetchJson(){
+    fetch("https://tender-teal-panda.cyclic.app")
       .then((response) => response.json())
-      .then((json) => setData(json.pilots))
+      .then((json) => setData(json))
       .catch((error) => console.error(error))
-  }, []);
-
-
-  function getIcon(_iconSize){
-    return L.icon({
-      iconUrl: require("./img/point.png"),
-      iconSize: [_iconSize]
-    })
   }
 
+
+
+  React.useEffect(() => {
+    fetchJson();
+    const interval = setInterval(() => {
+      fetchJson()
+    }, 60000); 
+    return () => clearInterval(interval);     
+  }, []);
 
   
   
   return (   
+
 
     <MapContainer center={[-35.52, 5.34]} zoom={3} minZoom={2} scrollWheelZoom={true} style={{ width:'100vw', height:'100vw'}}>
       <TileLayer
@@ -42,7 +40,6 @@ function App() {
           url="https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=O1Bqw80hgGnLYmBXIA59"
           attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
         />
-        <Marker position={[51.505, -0.09]} icon={getIcon(7)}></Marker>
         <PlaneList pilots={pilots}/>      
     </MapContainer>
 
@@ -51,3 +48,4 @@ function App() {
 
 
 export default App;
+
