f15926e521b78dbfc08718034ce4eff6086681c9
Aneta Tschernay <86184090+antschernay@users.noreply.github.com>
2023-02-22 09:26:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
-import { MapContainer, TileLayer} from 'react-leaflet';
+import { Route, Routes} from 'react-router-dom';
+import { useState } from 'react';
 import '../node_modules/leaflet/dist/leaflet.css'
-import PlaneList from "./PlaneList";
+import './App.css';
+import Home from './components/Home/Home';
+import Navigation from './components/Navigation/Navigation';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
+
 
 
 
 function App() {
 
   const [pilots, setData] = useState([]);
- 
+  const [time, setTime] = useState(new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}));
 
+  
   function fetchJson(){
-    fetch("https://tender-teal-panda.cyclic.app")
+    fetch("http://localhost:3001")
       .then((response) => response.json())
       .then((json) => setData(json))
       .catch((error) => console.error(error))
   }
+  
+  
+  React.useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}));
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
 
 
 
@@ -24,24 +38,27 @@ function App() {
     fetchJson();
     const interval = setInterval(() => {
       fetchJson()
-    }, 90000); 
-    return () => clearInterval(interval);     
+    }, 60000); 
+    return () => clearInterval(interval);    
   }, []);
 
-  
+
+
+
   
   return (   
-
-
-    <MapContainer center={[-35.52, 5.34]} zoom={3} minZoom={2} scrollWheelZoom={true} style={{ width:'100vw', height:'100vw'}}>
-      <TileLayer
-        //attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        //url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          url="https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=O1Bqw80hgGnLYmBXIA59"
-          attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-        />
-        <PlaneList pilots={pilots}/>      
-    </MapContainer>
+    <>
+    
+      <Navigation dateTime={time} pilots={pilots.length}/>
+      <Routes>
+        <Route path='/' element={<Home pilots={pilots}/>} />
+        <Route path='/signin' element={<SignIn/>} />
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/profile' />
+      </Routes>
+           
+      
+    </>
 
   );
 }
