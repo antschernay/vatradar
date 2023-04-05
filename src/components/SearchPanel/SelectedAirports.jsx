ac063f55c802f429d40e22179796f83b176dbb9d
Aneta Tschernay <86184090+antschernay@users.noreply.github.com>
2023-04-05 08:04:19 +0200

    Update SelectedAirports.jsx

--- src/components/SearchPanel/SelectedAirports.jsx
+++ src/components/SearchPanel/SelectedAirports.jsx
@@ -2,36 +2,9 @@ import React from 'react';
 import { useState, useMemo } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown, faCaretUp, faXmark } from '@fortawesome/free-solid-svg-icons';
-import SelectedPlanes from './SelectedPlanes';
+import AirportInfo from './AirportInfo';
 
-const SelectedAirports = ({airports, pilots, accordionItem, setAccordionItem, handleDelete, setSelectedAirports, selectedFlight, setSelectedFlight, handleAddPlane}) => {
-
-
-    const [arrivals, setArrivals] = useState([]);
-    const [departures, setDepartures] = useState([]);
-    const [accordionFlight, setAccordionFlight] = useState("");
-
-    const convertToDegrees = (decimal) => {
-        const degrees = Math.floor(decimal);
-        const minutes = (decimal - degrees) * 60;
-        const seconds = (minutes - Math.floor(minutes)) * 60;
-        return `${degrees}° ${Math.floor(minutes)}' ${Math.floor(seconds)}"`;
-    }
-
-    const getFlights = (icao, lat, lon) => {
-        {pilots.forEach(pilot => {
-            if (pilot.arr===icao){
-                setArrivals(arrivals => [...arrivals, {...pilot, "latAir": lat, "lonAir":lon}])
-              
-            }
-            if (pilot.dep===icao) {
-                setDepartures(departures => [...departures, {...pilot, "latAir": lat, "lonAir":lon}])
-            }
-        })};
-    }
-
-  
-
+const SelectedAirports = ({airports, pilots, accordionItem, setAccordionItem, handleDelete, setSelectedAirports, selectedFlight, setSelectedFlight, handleAddPlane, selectedAirport}) => {
 
     return (
         <div>
@@ -43,21 +16,17 @@ const SelectedAirports = ({airports, pilots, accordionItem, setAccordionItem, ha
                 
                 <>
 
-                    
                 
-                    <div className="pa2 ba b--silver w-100 mt2" style={{backgroundColor:'#7C90A3'}}>
+                    <div className="pa2 ba b--silver w-100 mt2" style={{backgroundColor: selectedAirport.icao_code===airport.icao_code ?'#999999' :'#7C90A3'}}>
                         <div className="flex justify-between">
                             <p className="code f6 white ma0"><b>{airport.icao_code}</b>, {airport.city}</p>
                             <div className='flex'>
                                 {accordionItem===airport.icao_code ? 
                                     <FontAwesomeIcon className='white dib f7 ph2 pointer' icon={faCaretUp} 
-                                    onClick={()=> {setAccordionItem(""); setAccordionFlight("")
-                                    }} />
+                                    onClick={()=> setAccordionItem("")} />
                                     :
                                     <FontAwesomeIcon className='white dib f7 ph2 pointer' icon={faCaretDown} 
-                                    onClick={()=> {setArrivals([]); setDepartures([]); setAccordionItem(airport.icao_code) ;
-                                                    
-                                                getFlights(airport.icao_code, airport.lat_decimal, airport.lon_decimal)}}  />
+                                    onClick={()=> {setAccordionItem(airport.icao_code); console.log(airport)}}  />
                                 }
                                 <FontAwesomeIcon className='white dib f7 ph2 pointer' icon={faXmark} 
                                     onClick={()=> handleDelete(airports, airport, setSelectedAirports)}/>
@@ -66,44 +35,9 @@ const SelectedAirports = ({airports, pilots, accordionItem, setAccordionItem, ha
                     </div>
                     
                     {accordionItem===airport.icao_code ? 
-                    <>
-                      
-
-                        <div className='pa2 ba b--silver w-100' style={{backgroundColor:'#2E3B47'}}>
-                             <table className='tl'>
-                            <th className='b white f6'>
-                                <tr>
-                                <td>Name</td>
-                                <td className='fw1 pl2'>{airport.name}</td>
-                                
-                                </tr>
-                                <tr>
-                                <td>Country</td>
-                                <td className='fw1 pl2'>{airport.country}</td>
-                                
-                                </tr>
-                                <tr>
-                                <td>Altitude</td>
-                                <td className='fw1 pl2'>{airport.altitude} m</td>
-                                </tr>
-                                <tr>
-                                <td>Position</td>
-                                <td className='fw1 pl2'>{convertToDegrees(airport.lat_decimal)} {airport.lat_dir}, {convertToDegrees(airport.lon_decimal)} {airport.lon_dir}</td>
-                                </tr>
-                                
-                                <tr>
-                                <td>Weather</td>
-                                <td className='fw1 pl2'></td>
-                                </tr>
-                                                                 
-                                                               
-                            </th>                                                             
-                        </table>
-                        <p className="code f6 ml1 b white underline ">Arrivals</p>
-                        <SelectedPlanes planes={arrivals} pilots={pilots} accordionItem={accordionFlight} setAccordionItem={setAccordionFlight} selectedFlight={selectedFlight} setSelectedFlight={setSelectedFlight} cardType={"arrival"} handleFunction={handleAddPlane} />
-                        <p className="code f6 b ml1 white underline">Departures</p>
-                        <SelectedPlanes planes={departures} pilots={pilots} accordionItem={accordionFlight} setAccordionItem={setAccordionFlight} selectedFlight={selectedFlight} setSelectedFlight={setSelectedFlight} cardType={"departure"} handleFunction={handleAddPlane}/>
-                        </div></>
+                        
+                            <AirportInfo airport={airport} pilots={pilots} selectedFlight={selectedFlight} setSelectedFlight={setSelectedFlight} handleAddPlane={handleAddPlane}/>
+                        
                     : null
 
                     }
@@ -117,12 +51,3 @@ const SelectedAirports = ({airports, pilots, accordionItem, setAccordionItem, ha
 };
 
 export default SelectedAirports;
-
-
-/*{pilots.map((pilot, i) => {
-                            if ( airport.icao_code===pilot.arr){
-                                handleAdd(arrivals, pilot, setArrivals)
-                            }
-                            else if (airport.icao_codepilot.dep) {
-                                handleAdd(departures, pilot, setDepartures)
-                        }})}*/
