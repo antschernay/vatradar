68b14b9bde96748fe6f7deecd4158e7cd04514d5
Aneta Tschernay <86184090+antschernay@users.noreply.github.com>
2023-06-20 11:14:07 +0200

    Update Controllers.jsx

--- src/components/SearchPanel/Controllers.jsx
+++ src/components/SearchPanel/Controllers.jsx
@@ -5,22 +5,22 @@ const Controllers = ({controllers}) => {
 
     return (
        
-        <table className="white code  w-100 center atc-table ">
-            <tr className="bg-gray">
-                <th className="ma0 pa2 f6">Callisgn</th>
-                <th className="ma0 pa2 f6">Name</th>
-                <th className="ma0 pa2 f6">Frequency</th>
-                <th className="ma0 pa2 f6">Since</th>
+        <table className="white code w-100 center atc-table ba b--silver collapse">
+            <tr className="bg-mid-gray bb b--silver items-center tc">
+                <th className="ma0 pa1 br b--silver f6">Callisgn</th>
+                <th className="ma0 pa1 br b--silver f6">Name</th>
+                <th className="ma0 pa1 br b--silver f6">Frequency</th>
+                <th className="ma0 pa1 br b--silver f6">Since</th>
             </tr>
         
             {controllers.map(controller => {
                 return (
         
-                <tr className="items-center tc bg-black">
-                    <td className="ma0 pa2 f6">{controller.callsign}</td>
-                    <td className="ma0 pa2 f6">{controller.name}</td>
-                    <td className="ma0 pa2 f6">{controller.frequency}</td>
-                    <td className="ma0 pa2 f6">Online</td>
+                <tr className="items-center tc bt b--mid-gray" style={{backgroundColor: "#8C8C78"}}>
+                    <td className="ma0 pa1 br b--silver f6">{controller.callsign}</td>
+                    <td className="ma0 pa1 br b--silver f6">{controller.name}</td>
+                    <td className="ma0 pa1 br b--silver f6">{controller.frequency}</td>
+                    <td className="ma0 pa1 br b--silver f6">12:57Z</td>
                 </tr>
             
             
