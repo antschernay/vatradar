53994473ef1db891b6852b19e539416dcf3f4a9b
Aneta Tschernay <86184090+antschernay@users.noreply.github.com>
2023-06-20 11:15:51 +0200

    Update Register.jsx

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -7,41 +7,49 @@ const Register = () => {
     const nameRef = useRef()
     const emailRef = useRef()
     const passwordRef = useRef()
+    const cidRef = useRef()
     const [error, setError] = useState('')
     const navigate = useNavigate()
 
     const handleSubmit = (e) => {
-        e.preventDefault()
-        try {
-            fetch('https://tender-teal-panda.cyclic.app/register', {
-                method: 'post',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({
-                    name: nameRef.current.value,
-                    email: emailRef.current.value,
-                    password: passwordRef.current.value
-                })
+        e.preventDefault();
+      
+        if (!nameRef.current.value || !emailRef.current.value || !passwordRef.current.value) {
+          setError("Fields name, email, and password cannot be empty");
+        } else {
+          fetch('https://tender-teal-panda.cyclic.app/register', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              name: nameRef.current.value,
+              email: emailRef.current.value,
+              password: passwordRef.current.value,
+              cid: cidRef.current.value
             })
+          })
             .then(response => response.json())
             .then(response => {
-                if (response==='User registered successfully!') {
-                    navigate('/signin', { replace: true })
-                } 
-                else setError('This email address is already used.')
+              if (response === 'User registered successfully!') {
+                navigate('/signin', { replace: true });
+              } else {
+                setError('This email address is already used.');
+              }
             })
-        } catch (error) {
-            setError('Something went wrong. Please try again.')
+            .catch(error => {
+              setError('Something went wrong. Please try again.');
+            });
         }
-        
-    }
+      };
 
 
     return (
-        <article className="br3 ba b--black-10 mv6 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <div className="pt6 code">
+        <article className="br3 ba b--black-10 w-100 w-50-m w-25-l shadow-5 center">
             <main className="pa4 black-80">
-                <div className="measure">
+                <div className="measure center">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="f4 tc fw6 ph0 mh0">Register</legend>
+                        <legend className="f2 b tc fw6 ph0 mh0">Register</legend>
+                        <p className="tc dark-red pv2">{error}</p>
                             <div className="mt3">
                                 <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
                                 <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
@@ -66,6 +74,14 @@ const Register = () => {
                                         name="password"  
                                         id="password"/>
                             </div>
+                            <div className="mv3">
+                                <label className="db fw6 lh-copy f6" htmlFor="cid">CID (optional)</label>
+                                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                        ref={cidRef}
+                                        type="text" 
+                                        name="cid"  
+                                        id="cid"/>
+                            </div>
 
                     </fieldset>
                     <div className="tc">
@@ -78,6 +94,7 @@ const Register = () => {
                 </div>
             </main>
         </article>
+      </div>
     )
   
 }
