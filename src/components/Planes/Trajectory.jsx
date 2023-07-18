aceb64b629b2f1a7cea425e1870d85dbb8dc5327
Aneta Tschernay <86184090+antschernay@users.noreply.github.com>
2023-07-18 07:31:58 +0200

    deleted /* section

--- src/components/Planes/Trajectory.jsx
+++ src/components/Planes/Trajectory.jsx
@@ -109,68 +109,6 @@ const Trajectory = ({callsign, lat, lon, arrIcao, depIcao}) => {
 export default Trajectory;
 
 
-/*useEffect(() => {
-    let arrAirportLong;
-    try {
-      fetch(`http://localhost:3001/airport/${arrIcao}`)
-      .then((response) => response.json())
-      .then((responseData) => {
-       
-        if (responseData.lat_decimal === 'NaN') {
-          setArrAirportLat(lat);
-        }
-        else if (responseData.lon_decimal === 'NaN') {
-          setArrAirportLon(lon);
-        }
-        else  {
-        setArrAirportLat(responseData.lat_decimal);
-        setArrAirportLon(responseData.lon_decimal);
-        arrAirportLong = responseData.lon_decimal;
-        }
-          
-      })
-  } 
-  catch (error) {
-   
-      console.error(error.message)
-  }
-  try {
-      fetch(`http://localhost:3001/airport/${depIcao}`)
-      .then((response) => response.json())
-      .then((responseData) => { 
-        console.log(responseData.lat_decimal)
-        if (responseData.lat_decimal === 'NaN') {
-          
-          setDepAirportLat(lat);
-        }
-        else if (responseData.lon_decimal === 'NaN') {
-          setDepAirportLon(lon);
-        }
-        else {
-        setDepAirportLat(responseData.lat_decimal);
-        setDepAirportLon(responseData.lon_decimal);
-        if (responseData.lon_decimal > 100 && arrAirportLong < -50 && lon < -50) {
-         
-          setDepAirportLon(responseData.lon_decimal - 360);
-          
-        }
-        else if (responseData.lon_decimal > 100 && arrAirportLong < -50) {
-          
-          setArrAirportLon(arrAirportLong + 360)
-          
-        }
-       
-        else if (responseData.lon_decimal < -50 && arrAirportLong > 100){
-          setArrAirportLon(arrAirportLong -360)
-        }
-        }
-      })
-      
-      
-  } catch (error) {
-   
-      console.log(error)
-  }
-  }, []);*/
+
 
 
